6e62aad5bd8d80b9461e805b2c0fa12889ce6053
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-09-17 06:09:56 +0200

    unfinished

---
+++ uno_attempt.py
@@ -0,0 +1,6 @@
+#Game preparations
+num_player = input("Enter the number of player :")
+num_player_int = int(num_player)
+if num_player_int >= 2:
+else:
+        print("Invalid number of players, you need at least 2 players to play UNO!. Are you lonely? how sad!!")
