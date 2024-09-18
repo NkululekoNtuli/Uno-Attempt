833e0d3588e0368308b9cc4299c1b023210312c4
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-09-18 06:09:41 +0200

    incomplete

--- uno_attempt.py
+++ uno_attempt.py
@@ -1,6 +1,20 @@
 #Game preparations
-num_player = input("Enter the number of player :")
+import random
+
+num_player = input("Enter number of player: ")
 num_player_int = int(num_player)
-if num_player_int >= 2:
-else:
-        print("Invalid number of players, you need at least 2 players to play UNO!. Are you lonely? how sad!!")
+count = 1
+list_a = []
+while count <= num_player_int:
+    count_str = str(count)
+    player_name = input("Enter player "+count_str+" name: ")
+    list_a.append(player_name)
+
+    count += 1
+print (list_a)
+
+def dealer_pick():
+    dealer = random.randrange(1,num_player_int)
+
+    return(list_a[dealer])
+print("The dealer is ",dealer_pick())
